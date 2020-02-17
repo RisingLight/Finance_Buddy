79b87d6c7b3e3a55b4f18230b2539f99fc9e0a7f
Abhinay kumar <abhinayavi@users.noreply.github.com>
2020-02-17 11:41:02 +0530

    added permission

--- app/src/main/java/tech/risinglight/financebuddy/view/SplashActivity.kt
+++ app/src/main/java/tech/risinglight/financebuddy/view/SplashActivity.kt
@@ -1,12 +1,21 @@
 package tech.risinglight.financebuddy.view
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.READ_SMS)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(,
+                arrayOf(Manifest.permission.READ_SMS))
+        }
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
