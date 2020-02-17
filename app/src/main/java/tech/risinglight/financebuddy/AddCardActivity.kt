6653c2d082473361e666498430be1afe33c0dd02
Abhishek Kumar <abhishek.kumar.7679@gmail.com>
2020-02-17 10:34:04 +0530

    upi added

---
+++ app/src/main/java/tech/risinglight/financebuddy/AddCardActivity.kt
@@ -0,0 +1,12 @@
+package tech.risinglight.financebuddy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddCardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_card)
+    }
+}
---
+++ app/src/main/java/tech/risinglight/financebuddy/UpiPaymentActivity.kt
@@ -0,0 +1,32 @@
+package tech.risinglight.financebuddy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.shreyaspatil.EasyUpiPayment.EasyUpiPayment
+import kotlinx.android.synthetic.main.activity_upi_payment.*
+import kotlin.random.Random
+
+
+class UpiPaymentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upi_payment)
+
+        payBtn.setOnClickListener {
+            val amount =
+                if (amountET.text.toString().contains('.')) amountET.text.toString() else amountET.text.toString() + ".00"
+            val random = Random(1100000)
+            val easyUpiPayment = EasyUpiPayment.Builder()
+                .with(this)
+                .setPayeeVpa(upi_idET.text.toString())
+                .setPayeeName("")
+                .setTransactionId(random.nextInt().toString())
+                .setTransactionRefId(random.nextInt().toString())
+                .setDescription(noteET.text.toString())
+                .setAmount(amount)
+                .build()
+            easyUpiPayment.startPayment()
+        }
+    }
+}
